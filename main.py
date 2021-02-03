92919a2a77cd551ead58044296e90f2593662c2e
Manuel Carvalho <burrowsrd@gmail.com>
2021-02-03 16:29:48 +1100

    added Pandas example

--- main.py
+++ main.py
@@ -13,37 +13,56 @@ import pandas
 #
 #     print(temperature)
 
-data = pandas.read_csv("data.csv")
-print(data["temp"])
+# data = pandas.read_csv("data.csv")
+# print(data["temp"])
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# print(data["temp"].mode())
+# print(data.condition)
+#
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+#
+# print(int(monday.temp) * 9 / 5 + 32)
+#
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
 
-data_dict = data.to_dict()
-print(data_dict)
+# data_frame = pandas.DataFrame(data_dict)
+# print(data_frame)
+#
+# data_frame.to_csv("new_data.csv")
 
-temp_list = data["temp"].to_list()
-print(temp_list)
-
-average = sum(temp_list) / len(temp_list)
-print(average)
-
-print(data["temp"].mean())
-print(data["temp"].max())
-print(data["temp"].mode())
-print(data.condition)
-
-print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
-print(monday.condition)
-
-print(int(monday.temp) * 9 / 5 + 32)
+data = pandas.read_csv("squirrel.csv")
+grey_squirrels = data[data["Primary Fur Color"] == "Gray"]
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
 
-data_frame = pandas.DataFrame(data_dict)
-print(data_frame)
+print(data_dict)
 
-data_frame.to_csv("new_data.csv")
+df = pandas.DataFrame(data_dict)
+df.to_csv("sqirrel_count.csv")
